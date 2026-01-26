59de04bd07d79151e6e5fc42f688d46597a540f3
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2026-01-26 17:43:50 +0000

    Add game config and rectangle structs

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,57 @@
 const zig_invaders = @import("zig_invaders");
 const rl = @import("raylib");
 
+const Rect = struct {
+    x: f32,
+    y: f32,
+    width: u32,
+    height: u32, // used unsigned 32 bit here to see if it works. Original tutorial used f32
+
+    pub fn intersects(self: Rect, other: Rect) bool {
+        return self.x < other.x + other.width and
+            other.x < self.x + self.width and
+            self.y < other.y + other.height and
+            other.y < self.x + self.height;
+    }
+};
+
+const PlayerConfig = struct {
+    width: f32,
+    height: f32,
+    startY: f32,
+};
+
+const BulletConfig = struct {
+    width: f32,
+    height: f32
+};
+const ShieldConfig = struct {
+    width: f32,
+    height: f32,
+    spacing: f32
+};
+
+const InvaderConfig = struct {
+    width: f32,
+    height: f32,
+    spacingX: f32,
+    spacingY: f32
+};
+
+const GameConfig = struct {
+    screenWidth: i32,
+    screenHeight: i32,
+    playerConfig: PlayerConfig,
+    bulletConfig: BulletConfig,
+    shieldConfig: ShieldConfig,
+    invaderConfig: InvaderConfig
+};
+
 pub fn main() !void {
 
     // init window
-    const screenWidth = 800;
-    const screenHeight = 600;
+    const screenWidth = 1280;
+    const screenHeight = 760;
 
     rl.initWindow(screenWidth, screenHeight, "Zig Invaderz");
     defer rl.closeWindow();
@@ -19,7 +65,7 @@ pub fn main() !void {
 
         const splashFontSize = screenHeight / 10;
         const splashText = "Zig Invaderz";
-        const splashTextWidth = rl.measureText(splashText,  splashFontSize);
+        const splashTextWidth = rl.measureText(splashText, splashFontSize);
         const splashPosX = screenWidth / 2 - @divFloor(splashTextWidth, 2);
         const splashPosY = screenHeight / 2 - splashFontSize / 2;
         rl.drawText(splashText, splashPosX, splashPosY, splashFontSize, rl.Color.green);
