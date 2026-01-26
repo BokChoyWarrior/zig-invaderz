e2d19961d75e1dcfbaf21d495600d3e122233756
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2026-01-26 18:25:48 +0000

    Add more game configs and initialisers

--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,8 @@ const rl = @import("raylib");
 const Rect = struct {
     x: f32,
     y: f32,
-    width: u32,
-    height: u32, // used unsigned 32 bit here to see if it works. Original tutorial used f32
+    width: f32,
+    height: f32,
 
     pub fn intersects(self: Rect, other: Rect) bool {
         return self.x < other.x + other.width and
@@ -20,23 +20,72 @@ const PlayerConfig = struct {
     width: f32,
     height: f32,
     startY: f32,
+
+    pub fn init(width: f32, height: f32, startY: f32) @This() {
+        return .{
+            .width = width,
+            .height = height,
+            .startY = startY,
+        };
+    }
+
+    pub fn fromScreenDims(screenWidth: f32, screenHeight: f32) @This() {
+        const defaultSizeModifier = 1 / 50;
+        const width = screenWidth * defaultSizeModifier / 2;
+        return .{
+            .width = width,
+            .height = screenHeight * defaultSizeModifier,
+            .startY = screenHeight * (1 - defaultSizeModifier),
+        };
+    }
 };
 
 const BulletConfig = struct {
     width: f32,
-    height: f32
+    height: f32,
+
+    pub fn init(width: f32, height: f32) @This() {
+        return .{ .width = width, .height = height };
+    }
+
+    pub fn fromScreenDims(screenWidth: f32) @This() {
+        const defaultScalar = 1 / 100;
+        return .{
+            .width = screenWidth * defaultScalar,
+            .height = screenWidth * defaultScalar * 2,
+        };
+    }
 };
 const ShieldConfig = struct {
     width: f32,
     height: f32,
-    spacing: f32
+    spacing: f32,
+
+    pub fn fromScreenDims(screenWidth: f32) @This() {
+        const defaultScalar = 1 / 10;
+        return .{
+            .width = screenWidth * defaultScalar,
+            .height = screenWidth * defaultScalar,
+            .spacing = screenWidth * defaultScalar,
+        };
+    }
 };
 
 const InvaderConfig = struct {
     width: f32,
     height: f32,
     spacingX: f32,
-    spacingY: f32
+    spacingY: f32,
+
+    pub fn fromScreenDims(screenWidth: f32) @This() {
+        const defaultScalar = 1 / 30;
+        return .{
+            .width = screenWidth * defaultScalar * 2,
+            .height = screenWidth * defaultScalar,
+            .spacingX = screenWidth * defaultScalar,
+            .spacingY = screenWidth * defaultScalar,
+        };
+    }
 };
 
 const GameConfig = struct {
@@ -45,7 +94,17 @@ const GameConfig = struct {
     playerConfig: PlayerConfig,
     bulletConfig: BulletConfig,
     shieldConfig: ShieldConfig,
-    invaderConfig: InvaderConfig
+    invaderConfig: InvaderConfig,
+    pub fn fromScreenDims(screenWidth: f32, screenHeight: f32) @This() {
+        return .{
+            .screenWidth = screenWidth,
+            .screenHeight = screenHeight,
+            .playerConfig = PlayerConfig.fromScreenDims(screenWidth, screenHeight),
+            .bulletConfig = BulletConfig.fromScreenDims(screenWidth),
+            .shieldConfig = ShieldConfig.fromScreenDims(screenWidth),
+            .invaderConfig = InvaderConfig.fromScreenDims(screenWidth),
+        };
+    }
 };
 
 pub fn main() !void {
